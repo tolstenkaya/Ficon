efabfe0926fd7cf2acd8555651eb1aa445b010c6
tolstenkaya <o.tutova.82@gmail.com>
2023-04-05 14:53:20 +0300

    Create LoginPage ane  DataPAge

---
+++ AdminPanel/Views/SinInPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdminPanel.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для SinInPage.xaml
+    /// </summary>
+    public partial class SinInPage : Page
+    {
+        public SinInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
